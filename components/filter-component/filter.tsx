bc79ac63a18d45d145a7146e5648a243f61cc805
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2024-09-19 19:23:15 +0200

    create filter component

---
+++ components/filter-component/filter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Filter = () => {
+    return (
+        <div>
+            filter here
+        </div>
+    )
+}
+
+export default Filter
--- components/homepage/articles/articles.tsx
+++ components/homepage/articles/articles.tsx
@@ -1,4 +1,5 @@
 import CardComponent from '@/components/card';
+import Filter from '@/components/filter-component';
 import Container from '@/components/ui-components/containter';
 import { Article } from '@/utils/Types'
 import React from 'react'
@@ -8,13 +9,14 @@ interface Props {
 }
 
 const Articles: React.FC<Props> = ({ articles = [] }) => {
-    return (
+    return <>
+        <Filter />
         <Container className='!p-0 grid grid-cols-1 lg:grid-cols-2 w-full'>
+
             {articles.map((article) => (
                 <CardComponent article={article} />
             ))}
         </Container>
-    )
+    </>
 }
-
 export default Articles
