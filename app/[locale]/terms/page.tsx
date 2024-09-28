74a875f50d9ec86d94de00e5d92674a665b2b523
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2024-09-28 22:19:42 +0200

    update privacy and terms of conditions

--- app/[locale]/terms/page.tsx
+++ app/[locale]/terms/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+'use client'
+
+import Container from '@/components/ui-components/containter';
+import RichText from '@/components/ui-components/rich-text';
+import { GET_PAGE_DATA } from '@/graphql/queries';
+import { useQuery } from '@apollo/client';
+import React, { useState, useEffect } from 'react'
 
 const Terms = () => {
+    const { data, loading, error } = useQuery(GET_PAGE_DATA, {
+        variables: { slug: 'terms' },
+    });
+    const [item, setItem] = useState<{ title: string; slug: string; details: JSON } | null>(null)
+    useEffect(() => {
+        if (data) {
+            const fetchedItem = data.pageCollection.items[0]
+            setItem(fetchedItem)
+        }
+    }, [data])
     return (
-        <div>
-            terms and conditions
+        <div className='flex justify-center min-h-[78vh]'>
+            <Container className='w-full md:w-2/3'>
+                <RichText content={item?.details} />
+            </Container>
         </div>
     )
 }
