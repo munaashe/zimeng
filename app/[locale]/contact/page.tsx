65c735855556bc7293a0f43e5495664ec7b5b3db
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2024-09-30 16:59:59 +0200

    contact us page

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+'use client'
+
+import Container from '@/components/ui-components/containter';
+import RichText from '@/components/ui-components/rich-text';
+import { GET_PAGE_DATA } from '@/graphql/queries';
+import { useQuery } from '@apollo/client';
+import React, { useState, useEffect } from 'react'
 
 const Contact = () => {
+    const { data, loading, error } = useQuery(GET_PAGE_DATA, {
+        variables: { slug: 'contact' },
+    });
+    const [item, setItem] = useState<{ title: string; slug: string; details: JSON } | null>(null)
+    useEffect(() => {
+        if (data) {
+            const fetchedItem = data.pageCollection.items[0]
+            setItem(fetchedItem)
+        }
+    }, [data])
     return (
-        <div>
-            Contact us
+        <div className='flex justify-center  min-h-[78vh]'>
+            <Container className='w-full md:w-2/3'>
+                <RichText content={item?.details} />
+            </Container>
         </div>
     )
 }
